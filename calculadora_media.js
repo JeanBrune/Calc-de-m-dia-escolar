480730f87c5a7d8482d4c7007bcd8b29b032934c
Jean Brune <95942357+JeanBrune@users.noreply.github.com>
2021-12-11 00:11:12 -0300

    added onInput function to limit keyboard typing

--- calculadora_media.js
+++ calculadora_media.js
@@ -1,26 +1,40 @@
-function media() {
+//função de onInput pra limitar os numeros inseridos por teclado.
+function algarismo() {
     //buscar as notas inseridas pelos inputs do html
-    var bimestre1 = parseFloat(document.getElementById("nota01").value)
-    var bimestre2 = parseFloat(document.getElementById("nota02").value)
-    var bimestre3 = parseFloat(document.getElementById("nota03").value)
-    var bimestre4 = parseFloat(document.getElementById("nota04").value)
+    let bimestre1 = parseFloat(document.getElementById("nota01").value)
+    let bimestre2 = parseFloat(document.getElementById("nota02").value)
+    let bimestre3 = parseFloat(document.getElementById("nota03").value)
+    let bimestre4 = parseFloat(document.getElementById("nota04").value)
 
-    //se digitarem nota maior do que 10
+    //caso digitem numero maior do que 10
     if (bimestre1 > 10) {
-        bimestre1 = 10
-    }
-    if (bimestre2 > 10) {
-        bimestre2 = 10
-    } 
-    if (bimestre3 > 10) {
-        bimestre3 = 10
-    }
-    if (bimestre4 > 10) {
-        bimestre4 = 10
+        document.getElementById("nota01").value = 10
+        console.log(bimestre1 + " convertido para a nota máxima (nota 10)")
     }
 
+    if (bimestre2 > 10) {
+        document.getElementById("nota02").value = 10
+        console.log(bimestre1 + " convertido para a nota máxima (nota 10)")
+    }
+
+    if (bimestre3 > 10) {
+        document.getElementById("nota03").value = 10
+        console.log(bimestre1 + " convertido para a nota máxima (nota 10)")
+    }
+
+    if (bimestre4 > 10) {
+        document.getElementById("nota04").value = 10
+        console.log(bimestre1 + " convertido para a nota máxima (nota 10)")
+    }
+}
+
+function media() {
+    let bimestre1 = parseFloat(document.getElementById("nota01").value)
+    let bimestre2 = parseFloat(document.getElementById("nota02").value)
+    let bimestre3 = parseFloat(document.getElementById("nota03").value)
+    let bimestre4 = parseFloat(document.getElementById("nota04").value)
     //tirar a média simples das notas inseridas
-    var media_anual = ((bimestre1 + bimestre2 + bimestre3 + bimestre4) / 4)
+    let media_anual = ((bimestre1 + bimestre2 + bimestre3 + bimestre4) / 4)
     console.log("1º bimestre: " + bimestre1 +
         "\n2º bimestre: " + bimestre2 +
         "\n3º bimestre: " + bimestre3 +
@@ -42,4 +56,3 @@ function media() {
         document.getElementById("resultado").innerHTML = "Erro. Preencha todos os campos e tente novamente."
     }
 }
-
